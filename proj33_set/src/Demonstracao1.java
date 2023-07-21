67309a6ba64e8522cfc12de53e55e784fd388da2
Eduardo Souza Pereira <eduardosouzapereira70@gmail.com>
2023-07-21 08:56:21 -0300

    Atualizações de diretório e adição de conteúdo

---
+++ proj33_set/src/Demonstracao1.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demonstracao1 {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+        System.out.println(set.contains("Notebook"));
+        for (String p : set) {
+            System.out.println(p);
+        }
+    }
+}
